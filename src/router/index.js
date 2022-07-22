c1b5df92df6081a37ed9746de693f47a1d1afb92
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2022-07-22 21:54:46 +0300

    connect view for main page

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'some',
-      component: () => import('../views/Some.vue')
+      name: 'main',
+      component: () => import('../views/main/Index.vue')
     },
     {
-      path: '/else',
-      name: 'else',
-      component: () => import('../views/Else.vue')
-    }
+      path: '/products',
+      name: 'products',
+      component: () => import('../views/product/Index.vue')
+    },
   ]
 })
 
---
+++ src/views/product/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Продукты
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  mounted() {
+    $(document).trigger('change')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
